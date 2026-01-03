3da039009bab7e31d2670cceb61058a19009213b
htquyen1994 <hothanhquyenbkhcm@gmail.com>
2026-01-03 22:23:55 +0700

    test

---
+++ src/main/java/com/lilknig/emberapi/EmberApiApplication.java
@@ -0,0 +1,13 @@
+package com.lilknig.emberapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmberApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmberApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lilknig/emberapi/EmberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilknig.emberapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmberApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
